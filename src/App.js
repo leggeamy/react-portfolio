85d23ba410ea8325cf257c94c37454ade434c3e9
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2021-01-30 14:45:51 -0500

    contact form function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Nav from './components/Nav'
 import About from './components/About';
 import Portfolio from './components/Portfolio';
+import ContactForm from './components/Contact';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Portfolio></Portfolio>
         <About></About>
       </main>
